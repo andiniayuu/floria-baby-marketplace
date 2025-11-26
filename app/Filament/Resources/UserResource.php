afa467a406a77606407d32234a18d832ce7c8213
andiniayuu <andiniayuningsih753@gmail.com>
2025-11-26 21:38:18 +0700

    category & brand resource complate

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\FormsComponent;
+use Filament\Pages\Page;
+use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Livewire\Component;
+
+use function Symfony\Component\Clock\now;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            Forms\Components\textInput::make('name')
+                ->required(),
+
+            Forms\Components\textInput::make('email')
+                ->label('Email Address')
+                ->email()
+                ->maxLength(255)
+                ->unique(ignoreRecord:true)
+                ->required(),
+
+            Forms\Components\DateTimePicker::make('email_verified_at')
+                ->label('Email Verified At')
+                ->default(now()),
+              
+            Forms\Components\textInput::make('password')
+                ->password()
+                ->dehydrated(fn ($state) => filled($state))
+                ->required(fn (Page $livewire): bool => $livewire instanceof CreateRecord),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+
+                tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                
+                tables\Columns\TextColumn::make('email_verified_at')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\ViewAction::make(), 
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
