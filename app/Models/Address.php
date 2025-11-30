491aa5151c2788231a4cc5ac530845a345db3eab
andiniayuu <andiniayuningsih753@gmail.com>
2025-11-30 22:41:56 +0700

    fix address order

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -7,25 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    Use HasFactory;
+    use HasFactory;
 
     protected $fillable = [
-        'order_id', 
-        'first_name', 
-        'last_name', 
+        'order_id',
+        'first_name',
+        'last_name',
         'phone',
         'street_address',
         'city',
         'state',
-        'zip_kode'
+        'zip_code'
     ];
 
-    public function order() {
+    public function order()
+    {
         return $this->belongsTo(Order::class);
     }
 
-    public function getFullNameAttribute() {
+    public function getFullNameAttribute()
+    {
         return "{$this->first_name} {$this->last_name}";
     }
 }
-
