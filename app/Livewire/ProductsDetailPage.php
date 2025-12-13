778941fdf10d95c2f8c35d342ffdc381788ba8dd
andiniayuu <andiniayuningsih753@gmail.com>
2025-12-13 23:51:09 +0700

    fix: fix product image gallery

--- app/Livewire/ProductsDetailPage.php
+++ app/Livewire/ProductsDetailPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Product Detail - BabyShop')]
 class ProductsDetailPage extends Component
 {
+    public $slug;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.products-detail-page');
+        return view('livewire.products-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrfail(),
+        ]);
     }
 }
