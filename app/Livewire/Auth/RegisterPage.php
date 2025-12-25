a81d0e0aab67bf520e05d7d7cc4cc989fd3d4d89
andiniayuu <andiniayuningsih753@gmail.com>
2025-12-25 14:11:17 +0700

    feat(auth,ui): enable login & register and switch to light mode

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,46 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Title('Register')]
 
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    // Register User
+    public function save()
+    {
+        $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|min:6|max:255',
+        ]);
+
+        // Save to DB
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        // Login User
+        Auth::login($user);
+
+        // Redirect to Homepage
+        return Redirect()->intended();
+    }
+
+
+
     public function render()
     {
         return view('livewire.auth.register-page');
