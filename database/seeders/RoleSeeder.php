1795f9f370e94bfb10331eb1b668134382187fd1
andiniayuu <andiniayuningsih753@gmail.com>
2026-01-25 15:44:44 +0700

    Add role seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        // Seller (Approved)
+        User::create([
+            'name' => 'Seller Demo',
+            'email' => 'seller@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'seller',
+            'seller_status' => 'approved',
+            'shop_name' => 'Toko Demo',
+            'shop_description' => 'Ini adalah toko demo untuk testing',
+        ]);
+
+        // User Biasa
+        User::create([
+            'name' => 'User Demo',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'user',
+        ]);
+    }
+}
