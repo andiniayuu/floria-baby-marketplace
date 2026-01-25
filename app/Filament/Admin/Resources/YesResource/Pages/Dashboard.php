ec39914cc90bef2bb05245a3f55baef1c7cf8363
andiniayuu <andiniayuningsih753@gmail.com>
2026-01-25 15:59:04 +0700

    chore: update dependencies and add admin dashboard pages

---
+++ app/Filament/Admin/Resources/YesResource/Pages/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\YesResource\Pages;
+
+use App\Filament\Admin\Resources\YesResource;
+use Filament\Actions;
+use Filament\Resources\Pages\page;
+
+class Dashboard extends page
+{
+    protected static string $resource = YesResource::class;
+}
